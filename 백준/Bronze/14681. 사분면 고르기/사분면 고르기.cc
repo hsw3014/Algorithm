fb01261656f5cf9c46113757f875b71dacaa97f2
hsw3014 <hsw3014@naver.com>
2022-05-10 15:56:46 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void solution()
+{
+	int x, y;
+	cin >> x >> y;
+	if (x < 0 && y < 0) cout << "3";
+	else if (x < 0) cout << "2";
+	else if (y < 0) cout << "4";
+	else cout << "1";
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
