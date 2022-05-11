0d678003c78af4f1932051bfef5c240f21957594
hsw3014 <hsw3014@naver.com>
2022-05-11 12:34:32 +0900

    [Bronze III] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void solution()
+{
+	int n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << endl;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
