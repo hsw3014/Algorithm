0a07ba1ad92685e845c26271c8f9fc0aa0adb531
hsw3014 <hsw3014@naver.com>
2022-05-17 11:27:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+bool cmp(const pair <int, int>& a, const pair <int, int>& b)
+{
+	if (a.second == b.second) return a.first < b.first;
+	return a.second < b.second;
+}
+
+void solution()
+{
+	int n, a, b;
+	vector < pair < int, int > > v;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		v.push_back({ a, b });
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
