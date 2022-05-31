6b5f13cc1fce2424baaf220fab73ca49a75cfa27
hsw3014 <hsw3014@naver.com>
2022-05-31 10:10:10 +0900

    [Silver III] Title: N과 M (3), Time: 376 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+bool visit[10] = { 0 };
+vector <int> v;
+
+void backtrack(int cnt)
+{
+	//다 채웠으면 출력
+	if (v.size() == m) {
+		for (int i = 0; i < m; i++) {
+			cout << v[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	//백트래킹
+	for (int i = 1; i <= n; i++) {
+		if (true) {
+			visit[i] = 1;
+			v.push_back(i);
+			backtrack(v.size());
+			visit[i] = 0;
+			v.pop_back();
+		}
+	}
+}
+
+void solution()
+{
+	cin >> n >> m;
+
+	backtrack(0);
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
