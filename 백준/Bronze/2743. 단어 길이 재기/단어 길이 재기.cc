7e2ff46d07aa862b8e25c08cac6e36984b20e0de
hsw3014 <hsw3014@naver.com>
2022-06-23 12:59:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void solution()
+{
+	string str;
+	cin >> str;
+	cout << str.length();
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
