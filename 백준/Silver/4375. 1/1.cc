a398ce1550ac0d97c49b65acf656bceb10ff5dd2
hsw3014 <hsw3014@naver.com>
2022-09-05 14:17:04 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+void solution()
+{
+
+	while (cin >> n) {
+		unsigned long long num = 1;
+		int res = 1;
+
+		while (num % n != 0) {
+			num %= n;
+			num = num * 10 + 1;
+			res++;
+		}
+
+		cout << res << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solution();
+}
